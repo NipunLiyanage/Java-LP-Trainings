223597b2042933d24dc95672936717e6a19df86d
Nipun Liyanage <51255650+NipunLiyanage@users.noreply.github.com>
2022-07-19 17:14:52 +0530

    Add files via upload

---
+++ Design Patterns/Prototype/Factory/ComboType.java
@@ -0,0 +1,5 @@
+package com.Factory;
+
+public enum ComboType {
+    BURGER,WITHDRINK,MEAL,COMBOMEAL
+}
---
+++ Design Patterns/Prototype/Factory/Drink.java
@@ -0,0 +1,31 @@
+package com.Factory;
+
+public class Drink extends Food {
+
+    private String size;
+    private String brand;
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return "Drink{" +
+                "size='" + size + '\'' +
+                ", brand='" + brand + '\'' +
+                '}';
+    }
+}
